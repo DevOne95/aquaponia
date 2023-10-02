b17b362a0ddb5f9d7848dae14838e2ffe31923d2
DevOne95 <tristan.zcmc@gmail.com>
2023-10-02 17:12:54 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:aquaponia/Home/dashboard.dart';
 import 'package:aquaponia/Home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
